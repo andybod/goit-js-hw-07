77bc34f59823e849d8272e4b7235482432c533cd
Andriy Bodnar <sokilets3@gmail.com>
2023-01-20 23:08:31 +0200

    end home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarKup = createGaleryItemMarKup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarKup);
+galleryEl.addEventListener("click", openModalItemGallery);
+
+function createGaleryItemMarKup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+		<a class="gallery__link" href=${original}>
+		  <img
+			 class="gallery__image"
+			 src=${preview}
+			 data-source= ${original}
+			 alt= ${description}
+		  />
+		</a>
+		</div>`;
+    })
+    .join("");
+}
+
+function openModalItemGallery(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  evt.preventDefault();
+  const bigImg = basicLightbox.create(
+    `
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {
+        galleryEl.addEventListener("keydown", function OnEscKeyPress(e) {
+          if (e.code === "Escape") {
+            instance.close();
+            galleryEl.removeEventListener("keydown", OnEscKeyPress);
+            console.log(e);
+          }
+        });
+      },
+    }
+  );
+  bigImg.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarKup = createGaleryItemMarKup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarKup);
+galleryEl.addEventListener("click", openModalItemGallery);
+
+function createGaleryItemMarKup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+		<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+function openModalItemGallery(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  console.log(e.target.getAttribute("alt"));
+}
